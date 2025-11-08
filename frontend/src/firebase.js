4621849de4de677607ed5f22f8a5367729455749
Jashbindar <jashbindar55@gmail.com>
2025-11-08 15:05:04 +0800

    Fixed the fetching error
    Added catch function in the fetchISSLocation() and simpleCleanup()

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBG3-RttyWCo5eGJr2A9BqjtHfmqSJg5Xc",
-  authDomain: "iss-tracker-iotproject.firebaseapp.com",
-  projectId: "iss-tracker-iotproject",
-  storageBucket: "iss-tracker-iotproject.firebasestorage.app",
-  messagingSenderId: "1040715006355",
-  appId: "1:1040715006355:web:4f507f426aed682ed8a866",
-  measurementId: "G-75KB6BP13R"
+  apiKey: "AIzaSyDTwDHHc-sF53PrYZUD2pSxAah2yhGu4Qc",
+  authDomain: "satorbit-iss-dashboard.firebaseapp.com",
+  projectId: "satorbit-iss-dashboard",
+  storageBucket: "satorbit-iss-dashboard.firebasestorage.app",
+  messagingSenderId: "463603914935",
+  appId: "1:463603914935:web:c9b5c0576a6b8e35561532",
+  measurementId: "G-7BGTPJWNLW"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // 🔹 Temporary test: fetch data from Firestore
 async function testFirestoreFetch() {
